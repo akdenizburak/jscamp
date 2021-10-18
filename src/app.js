554219ad47d5db77ce21c09bda4d8a1fcf1051a1
Burak <akdenizburakk7@gmail.com>
2021-10-19 00:02:19 +0300

    18 Ekim Ders Sonu

---
+++ src/app.js
@@ -0,0 +1,24 @@
+console.log("Hello World")
+
+let dolarBugun=9.30
+
+let dolarDun=9.20
+dolarDun="Ankara"
+
+{
+    let dolarDun=9.10
+}
+console.log(dolarDun)
+
+const euroDun=11.2
+
+console.log(euroDun)
+
+let konutKredileri=["Konut Kredisi","Emlak Kredisi","Taşıt kredisi"]
+console.log("<ul>")
+for(let i=0;i<konutKredileri.length;i++)
+{
+    console.log("<li>"+konutKredileri[i]+"</li>")
+}
+
+console.log("</ul>")
